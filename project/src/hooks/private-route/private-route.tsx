a35a52a0f06c4ade2af491e0c5110313ef8b99f0
Denis Gutsal <denis.gutsal@mail.ru>
2021-10-06 21:03:51 +0300

    fix issues

---
+++ project/src/hooks/private-route/private-route.tsx
@@ -0,0 +1,13 @@
+import { Route, Redirect} from 'react-router-dom';
+import { PrivateRouteProps } from '../../types/types';
+import { ROUTE_PATH } from '../../constants/constant';
+
+function PrivateRoute({ ...rest }: PrivateRouteProps): JSX.Element {
+  return (
+    <Route {...rest}>
+      {rest.isAccess ? rest.children : <Redirect to={ROUTE_PATH.ERROR} />}
+    </Route>
+  );
+}
+
+export default PrivateRoute;
